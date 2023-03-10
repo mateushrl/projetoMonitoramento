78407f4ce64dbda5ddf1e419919f51a514b2c1c7
Mateus <mateushrl918@gmail.com>
2023-03-09 23:42:47 -0300

    criacao basica da task acelerometro

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 #include <Arduino.h>
+#include "freertos/FreeRTOS.h"
+
+TaskHandle_t xTaskHandleAcelerometro;
+
+// Protótipo das Tasks
+void vTaskAcelerometro(void *parameters);
+
 
 void setup() {
-  // put your setup code here, to run once:
+
+  while (!Serial)
+  {
+    ; /* Somente vai em frente quando a serial estiver pronta para funcionar */
+  }
+
+  xTaskCreate(vTaskAcelerometro, "Acelerometro", configMINIMAL_STACK_SIZE + 2048, NULL, 1, &xTaskHandleAcelerometro);
+
+
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+
+void vTaskAcelerometro(void *parameters)
+{
+
+while (1)
+{
+  
+}
+
 }
